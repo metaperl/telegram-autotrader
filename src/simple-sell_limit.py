263bdce86d62d8d32579b5bcd871deab6b64541d
Terrence Brannon <metaperl@gmail.com>
2020-04-03 03:48:27 -0400

    mv

---
+++ src/simple-sell_limit.py
@@ -0,0 +1,21 @@
+
+
+import setpath
+from DWX_ZeroMQ_Connector_v2_0_1_RC8 import DWX_ZeroMQ_Connector
+
+OP_BUY = 0
+OP_SELL = 1
+OP_BUY_LIMIT = 2
+OP_SELL_LIMIT = 3
+OP_BUY_STOP = 4
+OP_SELL_STOP = 5
+
+zmq = DWX_ZeroMQ_Connector()
+my_trade = zmq._generate_default_order_dict()
+my_trade['_type'] = OP_SELL_LIMIT
+my_trade['_symbol'] = 'CHFJPY'
+my_trade['_price'] = 110.400
+zmq._DWX_MTX_NEW_TRADE_(_order=my_trade)
+
+resp = zmq._thread_data_output
+print(f"response={resp}")
--- src/telegram_to_mt4.py
+++ src/telegram_to_mt4.py
@@ -5,14 +5,34 @@ main script to react to trade in Telegram channel and send to MT4.
 from pyrogram import Client
 import mt4
 import pyrogramconf
+import re
 
 
 app = Client("my_account", pyrogramconf.api_id, pyrogramconf.api_hash)
 
+SIGNAL_CHANNEL = 'PAID Subscribers Clone'
 
 @app.on_message()
 def my_handler(client, message):
     print(f"Message Received={message}")
+
+    chatroom_name = message.chat.title
+    if chatroom_name != SIGNAL_CHANNEL:
+        print(f"Message is not from {SIGNAL_CHANNEL}. Not parsing message.")
+        return
+
+    print(f"Chatroom title={message.chat.title}")
+    # match #SYS Coin at #Bittrex
+    re1 = re.compile(
+        r'#(\S+)\s+at\s+({})'.format(self.exchange_label),
+        re.IGNORECASE | re.MULTILINE | re.DOTALL
+    )
+
+    m = re1.search(message)
+    if m:
+        coin, exchange = m.groups()
+        return coin, exchange
+
 
 app.run()
 
