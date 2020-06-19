64d4ba9a60d53140ad81acf9cddc6bd8280c0171
Terrence Brannon <metaperl@gmail.com>
2020-06-19 16:31:05 -0400

    sync

---
+++ src/etc/order_send_limit.py
@@ -0,0 +1,110 @@
+import time
+import MetaTrader5 as mt5
+import conf
+
+# display data on the MetaTrader 5 package
+print("MetaTrader5 package author: ", mt5.__author__)
+print("MetaTrader5 package version: ", mt5.__version__)
+
+# establish connection to the MetaTrader 5 terminal
+if not mt5.initialize():
+    print("initialize() failed, error code =", mt5.last_error())
+    quit()
+
+# prepare the buy request structure
+symbol = "EURUSD"
+symbol_info = mt5.symbol_info(symbol)
+if symbol_info is None:
+    print(symbol, "not found, can not call order_check()")
+    mt5.shutdown()
+    quit()
+
+# if the symbol is unavailable in MarketWatch, add it
+if not symbol_info.visible:
+    print(symbol, "is not visible, trying to switch on")
+    if not mt5.symbol_select(symbol, True):
+        print("symbol_select({}}) failed, exit", symbol)
+        mt5.shutdown()
+        quit()
+
+lot = 0.01
+point = mt5.symbol_info(symbol).point
+price = mt5.symbol_info_tick(symbol).ask
+deviation = 20
+request = {
+    "action": mt5.TRADE_ACTION_DEAL,
+    "symbol": symbol,
+    "volume": lot,
+    "type": mt5.ORDER_TYPE_BUY,
+    "price": price,
+    "sl": price - 100 * point,
+    "tp": price + 100 * point,
+    "deviation": deviation,
+    "magic": 234000,
+    "comment": "python script open",
+    "type_time": mt5.ORDER_TIME_GTC,
+    "type_filling": mt5.ORDER_FILLING_FOK,
+}
+
+# send a trading request
+result = mt5.order_send(request)
+# check the execution result
+print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation));
+if result.retcode != mt5.TRADE_RETCODE_DONE:
+    print("2. order_send failed, retcode={}".format(result.retcode))
+    # request the result as a dictionary and display it element by element
+    result_dict = result._asdict()
+    for field in result_dict.keys():
+        print("   {}={}".format(field, result_dict[field]))
+        # if this is a trading request structure, display it element by element as well
+        if field == "request":
+            traderequest_dict = result_dict[field]._asdict()
+            for tradereq_filed in traderequest_dict:
+                print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+    print("shutdown() and quit")
+    mt5.shutdown()
+    quit()
+
+print("2. order_send done, ", result)
+print("   opened position with POSITION_TICKET={}".format(result.order))
+print("   sleep 2 seconds before closing position #{}".format(result.order))
+time.sleep(2)
+# create a close request
+position_id = result.order
+price = mt5.symbol_info_tick(symbol).bid
+deviation = 20
+request = {
+    "action": mt5.TRADE_ACTION_DEAL,
+    "symbol": symbol,
+    "volume": lot,
+    "type": mt5.ORDER_TYPE_SELL,
+    "position": position_id,
+    "price": price,
+    "deviation": deviation,
+    "magic": 234000,
+    "comment": "python script close",
+    "type_time": mt5.ORDER_TIME_GTC,
+    "type_filling": mt5.ORDER_FILLING_RETURN,
+}
+# send a trading request
+result = mt5.order_send(request)
+# check the execution result
+print("3. close position #{}: sell {} {} lots at {} with deviation={} points".format(position_id, symbol, lot, price,
+                                                                                     deviation));
+if result.retcode != mt5.TRADE_RETCODE_DONE:
+    print("4. order_send failed, retcode={}".format(result.retcode))
+    print("   result", result)
+else:
+    print("4. position #{} closed, {}".format(position_id, result))
+    # request the result as a dictionary and display it element by element
+    result_dict = result._asdict()
+    for field in result_dict.keys():
+        print("   {}={}".format(field, result_dict[field]))
+        # if this is a trading request structure, display it element by element as well
+        if field == "request":
+            traderequest_dict = result_dict[field]._asdict()
+            for tradereq_filed in traderequest_dict:
+                print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+
+# shut down connection to the MetaTrader 5 terminal
+mt5.shutdown()
---
+++ src/etc/pymt5adapter/pending_order.py
@@ -0,0 +1,52 @@
+import MetaTrader5 as mt5
+import pymt5adapter
+from pymt5adapter.order import Order
+from pymt5adapter.symbol import Symbol
+
+class MySymbol(Symbol):
+
+    def pips(self, amount):
+        return amount * 10 * self.point
+
+def buy_stop():
+    symbol_str = 'EURUSD'
+    symbol = MySymbol(symbol_str)
+
+    lot = 0.01
+    entry = symbol.ask + symbol.pips(50)
+    print(f"Entry {entry}")
+    sl = entry - symbol.pips(10)
+    tp = entry + symbol.pips(10)
+
+    deviation = 20
+
+    o = Order.as_buy_stop(symbol=symbol, volume=lot, price=entry, stoplimit=entry, sl=sl, tp=tp,
+                          deviation=deviation, type_filling=mt5.ORDER_FILLING_FOK)
+
+    r = o.send()
+    print(r)
+
+
+def buy_limit():
+    symbol_str = 'EURUSD'
+    symbol = MySymbol(symbol_str)
+
+    lot = 0.01
+    entry = symbol.ask - symbol.pips(50)
+    print(f"Entry {entry}")
+    sl = entry - symbol.pips(10)
+    tp = entry + symbol.pips(10)
+
+    deviation = 20
+
+    o = Order.as_buy_limit(symbol=symbol, volume=lot, price=entry, stoplimit=entry, sl=sl, tp=tp,
+                          deviation=deviation, type_filling=mt5.ORDER_FILLING_FOK)
+
+    r = o.send()
+    print(r)
+
+
+if __name__ == '__main__':
+    with pymt5adapter.connected(enable_real_trading=True):
+        buy_stop()
+        buy_limit()
